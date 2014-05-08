f484315fe7d8ed7d12dfec7907356d78c769929e
Soutaro Matsumoto <matsumoto@soutaro.com>
2014-05-09 02:42:23 +0900

    Add skip_before_filter

--- app/controllers/newrelics_controller.rb
+++ app/controllers/newrelics_controller.rb
@@ -1,4 +1,6 @@
 class NewrelicsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
   def create
     @notification = NewrelicNotification.new(api_key: ENV["NEWRELIC_API_KEY"], app_id: ENV["NEWRELIC_APP_ID"], user: params[:user], revision: params[:head_long], git_log: params[:git_log])
 
